446dbcd9b99d48414db69dfebc8f9fcbdd05a21f
lhaquoc <38935979+lhaquoc@users.noreply.github.com>
2021-06-29 08:17:45 +0700

    add adjustment method

--- DSLessons/Pointers.cpp
+++ DSLessons/Pointers.cpp
@@ -1,4 +1,50 @@
 #include <stdio.h>
+#include <math.h>
+template <class T>
+void swap(T *x, T *y)
+{
+    T u = *x;
+    *x = *y;
+    *y = u;
+}
+
+template <class T>
+void adjust(T *x, T *y)
+{
+    if(abs(*x) > fabs(*y)) 
+        swap(&(*x), &(*y));
+}
+
+int indexMinAbs(float a[], int n)
+{
+    int i = 1;
+    int idx = 0;
+    while(i < n) {
+        if(fabs(a[i]) < fabs(a[idx])))
+            idx = i;
+        i++;
+    }
+    return idx;
+}
+
+float* pointerMinAbs(float a[], int n) {
+    int i = 1;
+    int idx = 0;
+    while(i < n) {
+        if(fabs(a[i]) < fabs(a[idx])))
+            idx = i;
+        i++;
+    }
+    return &a[idx];
+}
+
+void UpperCase(unsigned char* pCh)
+{
+    if(*pCh >= 'a' && *pCh <= 'z')
+    {
+        *pCh -= 32;
+    }
+}
 
 int main()
 {
@@ -18,7 +64,13 @@ int main()
 
     p = &x;
     *(float*)p = 1.5f;
-    printf("%f", x);
+    printf("%f\n", x);
 
+    // chuyen doi chu thuong sang chu hoa
+    unsigned char ch;
+    printf("Input a character: ");
+    scanf("%c", &ch);
+    UpperCase(&ch);
+    printf("After to uppercase: %c\n", ch);
     return 0; 
 }
